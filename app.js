6ee7e2041fc27a55761e4c6287347a691319bb07
Anirudh <anirudh.19090@gmail.com>
2023-06-20 16:22:26 -0400

    new changes

---
+++ app.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import session from "express-session";
+import cors from 'cors'
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import AuthController from "./users/auth-controller.js";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
+
+const app = express()
+
+app.use(
+  session({
+    secret: "any string",
+    resave: false,
+    saveUninitialized: true,
+  })
+ );
+
+ app.use(
+  cors({
+    credentials: true,
+    origin: (origin, callback) => {
+      // Allow all origins
+      callback(null, true);
+    },
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Allow all CRUD operations
+    })
+  );
+
+app.use(express.json());
+
+const port = process.env.PORT || 4000;
+app.use(express.json());
+
+TuitsController(app);
+HelloController(app)
+UserController(app)
+AuthController(app);
+
+app.listen(process.env.PORT || 4000)
